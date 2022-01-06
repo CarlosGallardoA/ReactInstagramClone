a456c9dc177af6b9d99c600743faa38b9254719c
Carlos Gallardo <carlitosgallardo009@gmail.com>
2022-01-06 00:59:21 -0500

    finished auth with firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
+import UserContext from "./context/user";
+import useAuthListener from "./hooks/useAuthListener";
 const Login = lazy(() => import("./pages/Login"));
 const Signup = lazy(() => import("./pages/Signup"));
 const NotFound = lazy(() => import("./pages/NotFound.js"));
 const DashBoard = lazy(() => import("./pages/Dashboard"));
 function App() {
+  const { user } = useAuthListener();
   return (
-    <div>
-      <Router>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Routes>
-            <Route path={ROUTES.DASHBOARD} element={<DashBoard />} />
-            <Route path={ROUTES.LOGIN} element={<Login />} />
-            <Route path={ROUTES.SIGNUP} element={<Signup />} />
-            <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
-          </Routes>
-        </Suspense>
-      </Router>
-    </div>
+    <UserContext.Provider value={{ user }}>
+      <div>
+        <Router>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Routes>
+              <Route path={ROUTES.DASHBOARD} element={<DashBoard />} />
+              <Route path={ROUTES.LOGIN} element={<Login />} />
+              <Route path={ROUTES.SIGNUP} element={<Signup />} />
+              <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </Router>
+      </div>
+    </UserContext.Provider>
   );
 }
 
